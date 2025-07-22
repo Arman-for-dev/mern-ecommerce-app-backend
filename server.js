55821fa958bbe396a4ce1aec098cd91ea8449b6f
Arman-for-dev <armanfordev59@gmail.com>
2025-07-22 10:15:19 +0600

    added swagger

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
 import todoRoute from "./routes/todo.routes.js";
 import userRoute from "./routes/user.route.js";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./utils/swagger.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,10 +27,17 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(cookieParser());
 
 
-app.use("/service/todo", todoRoute)
+app.use("/api/todo", todoRoute)
 app.use("/api/user", userRoute);
+
+app.use("/todolist/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customSiteTitle: "Todo List Management API",
+}));
+
+mongoose.set("strictQuery", true);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
